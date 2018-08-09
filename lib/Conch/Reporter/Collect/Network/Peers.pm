0a85a9dd1f69deb6f2f2de46131d0d75845efca9
Bryan Horstmann-Allen <bdha@joyent.com>
2018-08-09 15:44:33 +0000

    First stab at LLDP. Needs much more work.

--- lib/Conch/Reporter/Collect/Network/Peers.pm
+++ lib/Conch/Reporter/Collect/Network/Peers.pm
@@ -13,6 +13,31 @@ sub collect {
 
 sub _peers {
 	my ($device) = @_;
+
+	my %vendors = (
+		arista => [ "" ],
+		dell   => [ "" ],
+	);
+
+	my $cmd;
+	if ( -f "/var/tmp/lldp.out" ) {
+		$cmd = `cat /var/tmp/lldp.out`;
+	} else {
+		# This can take up to 60s to run. It doesn't give us some useful
+		# information like peer_descr or peer_text, but neither does
+		# lldpneighbors.
+		$cmd = `/var/tmp/dc-standup/get_link_lldp.sh`;
+	}
+
+	# 8TYJRD2,ixgbe0,a0:36:9f:c0:fb:b8,TenGigabitEthernet 1/1,va1-3-d05-2
+
+	foreach my $line (split/\n/, $cmd) {
+		my ($serial, $iface, $mac, $port, $switch) = split/,/, $line;
+		$device->{interfaces}{$iface}{peer_port}   = $port;
+		$device->{interfaces}{$iface}{peer_switch} = $switch;
+		$device->{interfaces}{$iface}{peer_mac}    = $mac;
+	}
+
 	return $device;
 }
 
