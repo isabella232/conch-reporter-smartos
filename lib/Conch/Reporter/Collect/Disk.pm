449d053d8ffc5ea3186417fd0bc2f57123e68d51
Bryan Horstmann-Allen <bdha@joyent.com>
2018-08-19 20:15:50 +0000

    Include diskinfo collector

---
+++ lib/Conch/Reporter/Collect/Disk.pm
@@ -0,0 +1,18 @@
+package Conch::Reporter::Collect::Disk;
+
+use strict;
+use warnings;
+
+use Conch::Reporter::Collect::Disk::Diskinfo;
+use Conch::Reporter::Collect::Disk::Inventory;
+
+sub collect {
+	my ($device) = @_;
+
+	$device = Conch::Reporter::Collect::Disk::Diskinfo::collect($device);
+	$device = Conch::Reporter::Collect::Disk::Inventory::collect($device);
+
+	return $device;
+}
+
+1;
