841be36fdeaa4fd587cf47033465f07eb5da674b
Bryan Horstmann-Allen <bdha@joyent.com>
2018-08-24 14:17:14 +0000

    Dump version into tarball, make a Manta note

--- package.sh
+++ package.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 V=$( git show --oneline | head -1 | awk '{print $1}' )
+echo $V > VERSION.txt
+
 F="conch-reporter-smartos-$V.tar.gz"
 cd ../
 tar -czf $F conch-reporter-smartos
 echo "$PWD/$F"
+
+echo "Make a symlink wherever you're storing this on Manta:"
+echo "mln ~~/public/conch-reporter-smartos-latest.tar.gz ~~/public/$F"
