46fa819e8de40958cd00147b10a8ee3bc105c40a
Bryan Horstmann-Allen <bdha@joyent.com>
2018-08-24 04:09:58 +0000

    Cleanup hwgrok provider

--- lib/Conch/Reporter/Collect/hwgrok.pm
+++ lib/Conch/Reporter/Collect/hwgrok.pm
@@ -7,6 +7,7 @@ use Carp;
 use Path::Tiny;
 use JSON::PP;
 use Time::HiRes qw(usleep ualarm gettimeofday tv_interval);
+use File::stat;
 use IPC::Cmd qw[can_run run run_forked];
 
 # hwgrok is: https://github.com/joyent/hwgrok
@@ -31,30 +32,43 @@ sub _load_hwgrok_cache {
 	my $file = "/tmp/hwgrok.json";
 	my $fp   = path($file);
 
+	my $update_interval = 60 * 60; # Every hour
+
 	my $hwgrok;
 
-	# XXX Add regen interval per the cache-aware disk collectors.
+	my $t0 = [gettimeofday];
 	if ( -f $file ) {
-		print "=> Using existing hwgrok cache: ";
-		my $t0 = [gettimeofday];
-		my $json = $fp->slurp_utf8;
-		$hwgrok = decode_json $json;
-		my $elapsed = tv_interval ($t0);
-		printf "%.2fs\n", $elapsed;
+		my $sb = stat($file);
+		if ( (time() - $sb->mtime) > $update_interval) {
+			print "=> Updating existing hwgrok cache: ";
+			$hwgrok = _run_hwgrok();
+		} else {
+			print "=> Using existing hwgrok cache: ";
+			my $json = $fp->slurp_utf8;
+		}
 	} else {
 		print "=> hwgrok cache not found, creating: ";
-		my $t0 = [gettimeofday];
-
-		my $cmd = './bin/hwgrok | grep -v ^$';
-		my( $success, $error_message, $full_buf, $stdout_buf, $stderr_buf ) =
-			run( command => $cmd, verbose => 0, timeout => 30 );
-		if( $success ) {
-			$fp->spew_utf8($stdout_buf);
-			$hwgrok = decode_json $stdout_buf;
-			my $elapsed = tv_interval ($t0);
-			printf "%.2fs\n", $elapsed;
-		}
+		$hwgrok = _run_hwgrok();
 	}
+
+	my $elapsed = tv_interval ($t0);
+	printf "%.2fs\n", $elapsed;
+
+sub _run_hwgrok {
+	my $file = "/tmp/hwgrok.json";
+	my $fp   = path($file);
+
+	my $cmd = './bin/hwgrok | grep -v ^$';
+
+	my $buffer;
+	scalar run( command => $cmd,
+		verbose => 0,
+		buffer  => \$buffer,
+		timeout => 30 );
+	my $hwgrok = decode_json $buffer;
+	$fp->spew_utf8($buffer);
+	return $hwgrok;
+}
 
 	return $hwgrok;
 }
